56557648e2f0789c83472d669f04d51343d6e3f9
anyonees <batarinau@mail.ru>
2024-12-11 22:01:50 +0300

    added.buttons.navpanel

--- my-app1/src/App.js
+++ my-app1/src/App.js
@@ -1,18 +1,33 @@
 import "./App.css";
 import ItemList from "./components/itemList/itemList.jsx";
+import NavPanel from "./components/navPanel/navPanel.jsx";
 
 export default function App() {
   return (
-    <div className="App-header">
-      <header >
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-          crossorigin="anonymous"
+    <body className="Main-page">
+      <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossOrigin="anonymous"
         />
-        <ItemList itemData= {"Hello World"}/>
+    <div className="Nav-bar" >
+      <header className="App-header">
+        <div className="Logo-link"><NavPanel/></div>
+        <div className="Item-create"><NavPanel/></div>
+        <div className="Profile-button"><NavPanel/></div>
       </header>
     </div>
+    <div className="Item-section"> 
+      <ItemList itemData= {"Hello World"}/>
+      <ItemList itemData= {"привет мир"}/>
+      <ItemList itemData= {"Hello World"}/>
+      <ItemList itemData= {"привет мир"}/>
+      <ItemList itemData= {"Hello World"}/>
+      <ItemList itemData= {"привет мир"}/>
+      <ItemList itemData= {"Hello World"}/>
+      <ItemList itemData= {"привет мир"}/>
+    </div>
+    </body>
   );
 }
--- my-app1/src/components/itemList/itemList.jsx
+++ my-app1/src/components/itemList/itemList.jsx
@@ -1,25 +1,18 @@
-import { Button } from 'react-bootstrap';
+
+
 function ItemList({itemData}){
     console.log(itemData)
     return(
         <div className='Item-block'>
             <div className='Item-image'>
-
-
-
             </div>
             <div className='Item-title'>
             {itemData}
-
             </div>
             <div className='Item-text'>
             {itemData}
-
             </div>
-
-
-
         </div>
     )
 }
-export default ItemList;
+export default ItemList; 
