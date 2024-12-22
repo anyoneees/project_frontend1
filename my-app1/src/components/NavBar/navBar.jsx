385e928acf8c12da2c16f6732df2c10bd73da1f9
anyonees <batarinau@mail.ru>
2024-12-22 17:17:21 +0300

    edited.iconsNavBar

--- my-app1/src/components/NavBar/navBar.jsx
+++ my-app1/src/components/NavBar/navBar.jsx
@@ -5,19 +5,23 @@ function NavBar() {
       <header className="App-header">
         <div className="Logo-link">
           <a href="http://localhost:3000">
-            <img className="Logo" src="logo-image.png" />
+            <img className="Logo" src="circula.png" />
           </a>
         </div>
         <div className="Item-create">
-          <Button style={{ backgroundColor: "#73688D", borderColor: "#73688D" }}>Создать</Button>
+          <Button style={{ backgroundColor: "#73688D", borderColor: "#73688D" }}>
+             <img src="create.png" className="Create-pic"/></Button>
         </div>
         <div className="Chat-button">
           <a href="http://localhost:3000/chat">
-            <Button style={{ backgroundColor: "#73688D", borderColor: "#73688D" }}>Чат</Button>
+            <Button style={{ backgroundColor: "#73688D", borderColor: "#73688D" }}>
+               <img src="chat.png" className="Chat-pic"/></Button>
           </a>
         </div>
         <div className="Notification-button">
-          <Button style={{ backgroundColor: "#73688D", borderColor: "#73688D" }}>Уведомления</Button>
+          <Button style={{ backgroundColor: "#73688D", borderColor: "#73688D" }}>
+            <img src="notif2.png" className="Notification-pic"/>
+          </Button>
         </div>
         <div className="Profile-button">
           <a href="http://localhost:3000/profile">
